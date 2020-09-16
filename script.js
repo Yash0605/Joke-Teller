b857da7bdd4f687b6efc35ef24b33517548dcbd2
yash <hsay0605@gmail.com>
2020-09-16 20:56:42 +0530

    Final code commit

--- script.js
+++ script.js
@@ -1,118 +1,11 @@
 // apiKey = 5f2033a38bb24c149d79be937ac19471
-const buttonElement = document.getElementById('button');
-const audioElement = document.getElementById('audio')
+const buttonElement = document.getElementById("button");
+const audioElement = document.getElementById("audio");
 
-// VoiceRSS Javascript SDK
-const VoiceRSS = {
-  speech: function (e) {
-    this._validate(e), this._request(e);
-  },
-  _validate: function (e) {
-    if (!e) throw "The settings are undefined";
-    if (!e.key) throw "The API key is undefined";
-    if (!e.src) throw "The text is undefined";
-    if (!e.hl) throw "The language is undefined";
-    if (e.c && "auto" != e.c.toLowerCase()) {
-      var a = !1;
-      switch (e.c.toLowerCase()) {
-        case "mp3":
-          a = new Audio().canPlayType("audio/mpeg").replace("no", "");
-          break;
-        case "wav":
-          a = new Audio().canPlayType("audio/wav").replace("no", "");
-          break;
-        case "aac":
-          a = new Audio().canPlayType("audio/aac").replace("no", "");
-          break;
-        case "ogg":
-          a = new Audio().canPlayType("audio/ogg").replace("no", "");
-          break;
-        case "caf":
-          a = new Audio().canPlayType("audio/x-caf").replace("no", "");
-      }
-      if (!a) throw "The browser does not support the audio codec " + e.c;
-    }
-  },
-  _request: function (e) {
-    var a = this._buildRequest(e),
-      t = this._getXHR();
-    (t.onreadystatechange = function () {
-      if (4 == t.readyState && 200 == t.status) {
-        if (0 == t.responseText.indexOf("ERROR")) throw t.responseText;
-        // (audioElement.src = t.responseText), audioElement.play();
-        // new Audio(t.responseText).play();
-        audioElement.src = t.responseText;
-        audioElement.play();
-      }
-    }),
-      t.open("POST", "https://api.voicerss.org/", !0),
-      t.setRequestHeader(
-        "Content-Type",
-        "application/x-www-form-urlencoded; charset=UTF-8"
-      ),
-      t.send(a);
-  },
-  _buildRequest: function (e) {
-    var a = e.c && "auto" != e.c.toLowerCase() ? e.c : this._detectCodec();
-    return (
-      "key=" +
-      (e.key || "") +
-      "&src=" +
-      (e.src || "") +
-      "&hl=" +
-      (e.hl || "") +
-      "&r=" +
-      (e.r || "") +
-      "&c=" +
-      (a || "") +
-      "&f=" +
-      (e.f || "") +
-      "&ssml=" +
-      (e.ssml || "") +
-      "&b64=true"
-    );
-  },
-  _detectCodec: function () {
-    var e = new Audio();
-    return e.canPlayType("audio/mpeg").replace("no", "")
-      ? "mp3"
-      : e.canPlayType("audio/wav").replace("no", "")
-      ? "wav"
-      : e.canPlayType("audio/aac").replace("no", "")
-      ? "aac"
-      : e.canPlayType("audio/ogg").replace("no", "")
-      ? "ogg"
-      : e.canPlayType("audio/x-caf").replace("no", "")
-      ? "caf"
-      : "";
-  },
-  _getXHR: function () {
-    try {
-      return new XMLHttpRequest();
-    } catch (e) {}
-    try {
-      return new ActiveXObject("Msxml3.XMLHTTP");
-    } catch (e) {}
-    try {
-      return new ActiveXObject("Msxml2.XMLHTTP.6.0");
-    } catch (e) {}
-    try {
-      return new ActiveXObject("Msxml2.XMLHTTP.3.0");
-    } catch (e) {}
-    try {
-      return new ActiveXObject("Msxml2.XMLHTTP");
-    } catch (e) {}
-    try {
-      return new ActiveXObject("Microsoft.XMLHTTP");
-    } catch (e) {}
-    throw "The browser does not support HTTP request";
-  },
-};
-
-function test() {
+function tellMe(src) {
   VoiceRSS.speech({
     key: "5f2033a38bb24c149d79be937ac19471",
-    src: "Hello, world!",
+    src: src,
     hl: "en-us",
     v: "Linda",
     r: 0,
@@ -122,6 +15,32 @@ function test() {
   });
 }
 
-buttonElement.addEventListener('click', () => {
-    test();
-})
+// Get joke from a joke api
+async function getJokes() {
+  const jokeApi = "https://sv443.net/jokeapi/v2/joke/Programming,Miscellaneous,Pun?blacklistFlags=nsfw,sexist";
+  buttonElement.setAttribute('disabled', true)
+  try {
+    const joke = await fetch(jokeApi);
+    const jokeResponse = await joke.json();
+    if (jokeResponse) {
+      if(jokeResponse.error) {
+        throw jokeResponse.error;
+      } else {
+        const jokeText = jokeResponse.setup ? `${jokeResponse.setup} ... ${jokeResponse.delivery}` : jokeResponse.joke;
+        tellMe(jokeText);
+      }
+    } else {
+      throw "error";
+    } 
+    
+  } catch(e) {
+    console.log("Oopsie Woopsie guess the joke's on us " + e);
+  }
+}
+
+buttonElement.addEventListener("click", getJokes);
+
+// To keep track of when the joke has ended
+audioElement.addEventListener("ended", () => {
+  buttonElement.disabled = false;  
+});
